c4b9f8902df217645d0a40054949c2c3f86a4029
xionghao <xionghao930316@gmail.com>
2019-06-14 11:48:40 +0800

    initial

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ItemsViewController.swift
 //  Homepwner
 //
 //  Created by xionghao on 2019/6/14.
@@ -8,13 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
